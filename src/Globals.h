593db48cc331bfe06239d6fae5a1f33cfde876a7
MrCappuccino <matas234@gmail.com>
2016-03-03 16:21:14 +0100

    Removed extern where unrequired

--- src/Globals.h
+++ src/Globals.h
@@ -3,31 +3,31 @@
 using FPType = double;
 
 // 1920 x 1080p
-//extern constexpr FPType WIDTH = 1920;
-//extern constexpr FPType HEIGHT = 1080;
+// constexpr FPType WIDTH = 1920;
+// constexpr FPType HEIGHT = 1080;
 
 // 2560 x 1440p
-//extern constexpr FPType WIDTH = 2560;
-//extern constexpr FPType HEIGHT = 1440;
+// constexpr FPType WIDTH = 2560;
+// constexpr FPType HEIGHT = 1440;
 
 // 3840 x 2160 4k
-extern constexpr FPType WIDTH = 3840;
-extern constexpr FPType HEIGHT = 2160;
+// constexpr FPType WIDTH = 3840;
+// constexpr FPType HEIGHT = 2160;
 
 // 7680 x 4320 8k
-//extern constexpr FPType WIDTH = 7680;
-//extern constexpr FPType HEIGHT = 4320;
+// constexpr FPType WIDTH = 7680;
+// constexpr FPType HEIGHT = 4320;
 
 // 7680 x 4320 16k
-//extern constexpr FPType WIDTH = 15360;
-//extern constexpr FPType HEIGHT = 8640;
+constexpr FPType WIDTH = 15360;
+constexpr FPType HEIGHT = 8640;
 
-extern constexpr FPType ASPECT_RATIO = WIDTH / HEIGHT;
-extern constexpr FPType AMBIENT_LIGHT = 0.8;
-extern constexpr FPType TOLERANCE = 0.0000001;
-extern constexpr int SUPERSAMPLING = 4;
+constexpr FPType ASPECT_RATIO = WIDTH / HEIGHT;
+constexpr FPType AMBIENT_LIGHT = 0.8;
+constexpr FPType TOLERANCE = 0.0000001;
+constexpr int SUPERSAMPLING = 4;
 
-extern constexpr bool REFLECTIONS_ON = true;
-extern constexpr bool SPECULAR_ON = true;
-extern constexpr bool SHADOWS_ON = true;
-extern constexpr bool DIFFUSE_ON = true;
+constexpr bool REFLECTIONS_ON = true;
+constexpr bool SPECULAR_ON = true;
+constexpr bool SHADOWS_ON = true;
+constexpr bool DIFFUSE_ON = true;
